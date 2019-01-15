07f39fdecfb1b05e341bbab9e30bd66bdca71541
Zoltan Harmath <zolih@outlook.com>
2019-01-14 21:16:30 -0800

    First checkin

---
+++ LogoffUser/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogoffUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LockWorkStation();
+            ExitWindowsEx(20, 0);
+            //20 = EWX_FORCEIFHUNG (0x00000010) + EWX_FORCE (0x00000004)
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool ExitWindowsEx(uint uFlags, uint dwReason);
+        //https://docs.microsoft.com/en-us/windows/desktop/api/winuser/nf-winuser-exitwindowsex
+
+        [DllImport("user32.dll")]
+        public static extern bool LockWorkStation();
+        //https://docs.microsoft.com/en-us/windows/desktop/api/winuser/nf-winuser-lockworkstation
+
+    }
+}
